d4135a53e2e3942cde8b4ad9543e1a070806a7b6
Chris Sabin <christopher.d.sabin@gmail.com>
2020-05-15 22:54:11 -0700

    heap can now be added to, shown as a menu with values compared for heap rules or shown as an array, heapify function needs to be removed,

--- pyFiles/heapsOfFun.py
+++ pyFiles/heapsOfFun.py
@@ -1,1 +1,51 @@
-print('greetings from a different file')
+import heapq
+
+class myHeapThingy:
+    def __init__(self):
+        self.h = []
+        heapq.heapify(self.h)
+
+def addItem(h, intVal):
+    heapq.heappush(h, int(intVal))
+
+
+def customPopulate(h):
+    heapq.heappush(h, 1)
+    heapq.heappush(h, 3)
+    heapq.heappush(h, 2)
+    heapq.heappush(h, 0)
+    heapq.heappush(h, 5)
+    heapq.heappush(h, 1)
+    heapq.heappush(h, 4)
+    heapq.heappush(h, 3)
+
+def makeHeap(h):
+    heapq.heapify(h)
+
+def populateHeapSequence(h, numItems, isReversed):
+    if not reversed:
+        for i in range(numItems):
+            heapq.heappush(h, i)
+    else:
+        for i in reversed(range(numItems)):
+            heapq.heappush(h, i)
+    #heapq.heapify(h)
+
+
+def checkValues(h):
+    print("for each k, h[2 * k + 1] and h[2 * k + 2] are the children\n")
+    for k in range(len(h)):
+        print("Item {} ".format(k))
+        if 2 * k + 2 < len(h):
+            print("Value: {}".format(h[k]))
+            print("Right child: {} \nFollows heap rule: {}".format(int(h[2 * k + 2]), int(h[k]) <= int(h[2 * k + 2] )))
+            print("LEFT child: {} \nFollows heap rule: {}\n".format(int(h[2 * k + 1]), int(h[k]) <= int(h[2 * k + 1] )))
+        elif 2 * k + 1 < len(h):
+            print("Value: {}".format(h[k]))
+            print("LEFT child: {} \nFollows heap rule: {}\n".format(int(h[2 * k + 1]), int(h[k]) <= int(h[2 * k + 1])))
+        else:
+            print("Value: {}".format(h[k]))
+            print("No children\n")
+
+def printHeap(h):
+    print(h, "\n")
