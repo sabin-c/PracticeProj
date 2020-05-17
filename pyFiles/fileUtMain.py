c69f32e781be20677301127077de58ffa2187a55
Chris Sabin <christopher.d.sabin@gmail.com>
2020-05-17 10:35:58 -0700

    moving on from file utilities

--- pyFiles/fileUtMain.py
+++ pyFiles/fileUtMain.py
@@ -1,14 +1,13 @@
 import fileUtLib
-import os
 
 targetDirName = "../generatedFiles"
 fileExtension = "txt"
 
-def doStuff():
+def main():
     nm = "someName.txt"
     fileUtLib.assureFileExistence(nm)
 
 if __name__ == "__main__":
-    doStuff()
+    main()
 
 
